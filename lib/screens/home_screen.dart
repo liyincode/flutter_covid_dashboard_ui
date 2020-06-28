6eb58909b8163c32383704ca3ed6d6e90bd0a7b3
Shuaiyin <shuaiyin.top@foxmail.com>
2020-06-28 22:28:02 +0800

    finised some tasks

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,50 +1,219 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_covid_dashboard_ui/config/palette.dart';
+import 'package:flutter_covid_dashboard_ui/config/styles.dart';
+import 'package:flutter_covid_dashboard_ui/data/data.dart';
 import 'package:flutter_covid_dashboard_ui/widgets/custom_app_bar.dart';
 import 'package:flutter_covid_dashboard_ui/widgets/country_dropdown.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: CustomAppBar(),
       body: CustomScrollView(
         slivers: <Widget>[
-          SliverToBoxAdapter(
-            child: Container(
-              padding: const EdgeInsets.all(20.0),
-              decoration: BoxDecoration(
-                  color: Palette.primaryColor,
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(40.0),
-                      bottomRight: Radius.circular(40.0))),
-              child: Column(
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(
-                        'Covid-19',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 25.0,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      CountryDropdown(
-                        countries: ['CN', 'FR', 'IN', 'IT', 'UK', 'USA'],
-                        country: 'CN',
-                        onChanged: (val) {
+          _buildHeader(screenHeight),
+          _buildPreventionTips(screenHeight),
+          _buildYouOwnTest(screenHeight)
+        ],
+      ),
+    );
+  }
 
-                        },
-                      )
-                    ],
-                  )
-                ],
+  SliverToBoxAdapter _buildHeader(double screenHeight) {
+    return SliverToBoxAdapter(
+      child: Container(
+        padding: const EdgeInsets.all(20.0),
+        decoration: BoxDecoration(
+            color: Palette.primaryColor,
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(40.0),
+                bottomRight: Radius.circular(40.0))),
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'Covid-19',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                CountryDropdown(
+                  countries: ['CN', 'FR', 'IN', 'IT', 'UK', 'USA'],
+                  country: 'CN',
+                  onChanged: (val) {},
+                )
+              ],
+            ),
+            SizedBox(
+              height: screenHeight * 0.03,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Are you feeling sick?',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                SizedBox(
+                  height: screenHeight * 0.01,
+                ),
+                Text(
+                  'If you feel sick with any COVID-19 symptoms, please call or text us immediately for help',
+                  style: TextStyle(
+                    color: Colors.white60,
+                    fontSize: 12.0,
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: screenHeight * 0.03,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                FlatButton.icon(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 20.0,
+                    vertical: 10.0,
+                  ),
+                  onPressed: () {},
+                  color: Colors.red,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0)),
+                  icon: Icon(
+                    Icons.phone,
+                    color: Colors.white,
+                  ),
+                  label: Text(
+                    'Call Now',
+                    style: Styles.buttonTextStyle,
+                  ),
+                  textColor: Colors.white,
+                ),
+                FlatButton.icon(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 20.0,
+                    vertical: 10.0,
+                  ),
+                  onPressed: () {},
+                  color: Colors.blue,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0)),
+                  icon: Icon(
+                    Icons.chat_bubble,
+                    color: Colors.white,
+                  ),
+                  label: Text(
+                    'Send SMS',
+                    style: Styles.buttonTextStyle,
+                  ),
+                  textColor: Colors.white,
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  SliverToBoxAdapter _buildPreventionTips(double screenHeight) {
+    return SliverToBoxAdapter(
+      child: Container(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Prevention',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 22.0,
               ),
             ),
-          )
-        ],
+            SizedBox(
+              height: 12.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: prevention
+                  .map((e) => Column(
+                        children: <Widget>[
+                          Image.asset(
+                            e.keys.first,
+                            height: screenHeight * 0.12,
+                          ),
+                          Text(
+                            e.values.first,
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w500,
+                            ),
+                            textAlign: TextAlign.center,
+                          )
+                        ],
+                      ))
+                  .toList(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  SliverToBoxAdapter _buildYouOwnTest(double screenHeight) {
+    return SliverToBoxAdapter(
+      child: Container(
+        margin: EdgeInsets.symmetric(
+          vertical: 10.0,
+          horizontal: 20.0,
+        ),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: <Color>[Color(0xFFAD9FE4), Palette.primaryColor]),
+            borderRadius: BorderRadius.circular(20.0)),
+        height: screenHeight * 0.15,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            Image.asset('assets/images/own_test.png'),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Do your own test!',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  'Follow the instructions\nto do your own test.',
+                  style: TextStyle(color: Colors.white70,
+                  fontSize: 16.0),
+                  maxLines: 2,
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/screens/stats_screen.dart
+++ lib/screens/stats_screen.dart
@@ -1,8 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:bubble_tab_indicator/bubble_tab_indicator.dart';
 
-class StatsScreen extends StatelessWidget {
+import 'package:flutter_covid_dashboard_ui/config/palette.dart';
+import 'package:flutter_covid_dashboard_ui/config/styles.dart';
+import 'package:flutter_covid_dashboard_ui/widgets/custom_app_bar.dart';
+
+class StatsScreen extends StatefulWidget {
+  @override
+  _StatsScreenState createState() => _StatsScreenState();
+}
+
+class _StatsScreenState extends State<StatsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      backgroundColor: Palette.primaryColor,
+      appBar: CustomAppBar(),
+      body: CustomScrollView(
+        physics: ClampingScrollPhysics(),
+        slivers: <Widget>[
+          _buildHeader(),
+          _buildRegionTabBar(),
+          _buildStatsTabBar()
+        ],
+      ),
+    );
+  }
+
+  SliverPadding _buildHeader() {
+    return SliverPadding(
+      padding: EdgeInsets.all(20.0),
+      sliver: SliverToBoxAdapter(
+        child: Text(
+          'Statistics',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 25.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  SliverToBoxAdapter _buildRegionTabBar() {
+    return SliverToBoxAdapter(
+      child: DefaultTabController(
+        length: 2,
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 20.0),
+          height: 50.0,
+          decoration: BoxDecoration(
+              color: Colors.white24, borderRadius: BorderRadius.circular(25.0)),
+          child: TabBar(
+            indicator: BubbleTabIndicator(
+              tabBarIndicatorSize: TabBarIndicatorSize.tab,
+              indicatorHeight: 40.0,
+              indicatorColor: Colors.white,
+            ),
+            labelStyle: Styles.tabTextStyle,
+            labelColor: Colors.black,
+            unselectedLabelColor: Colors.white,
+            tabs: <Widget>[
+              Text('My Country'),
+              Text('Global'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  SliverPadding _buildStatsTabBar() {
+    return SliverPadding(
+      padding: EdgeInsets.all(20.0),
+      sliver: SliverToBoxAdapter(
+        child: DefaultTabController(
+          length: 3,
+          child: TabBar(
+            indicatorColor: Colors.transparent,
+            labelStyle: Styles.tabTextStyle,
+            labelColor: Colors.white,
+            unselectedLabelColor: Colors.white60,
+            tabs: <Widget>[
+              Text('Total'),
+              Text('Today'),
+              Text('Yesterday'),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
